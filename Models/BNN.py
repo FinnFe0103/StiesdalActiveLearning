cba8ce75f3716620ccbf00f21db9f437681858ae
FinnFe0103 <fife22ab@student.cbs.dk>
2024-03-15 14:19:38 +0100

    Framework Update

--- Models/BNN.py
+++ Models/BNN.py
@@ -5,16 +5,16 @@ from blitz.utils import variational_estimator
     
 @variational_estimator
 class BayesianNetwork(nn.Module):
-    def __init__(self, input_dim, hidden_size, layer_number):
+    def __init__(self, input_dim, hidden_size, layer_number, prior_sigma):
         super().__init__()
         
         self.layers = nn.ModuleList()  # A list to hold all layers
-        self.layers.append(BayesianLinear(input_dim, hidden_size, prior_sigma_1=0.001)) # Input layer
+        self.layers.append(BayesianLinear(input_dim, hidden_size, prior_sigma_1=prior_sigma)) # Input layer
         
         for _ in range(1, layer_number - 1):  # Add hidden layers based on layer_number
-            self.layers.append(BayesianLinear(hidden_size, hidden_size, prior_sigma_1=0.001))
+            self.layers.append(BayesianLinear(hidden_size, hidden_size, prior_sigma_1=prior_sigma))
         
-        self.layers.append(BayesianLinear(hidden_size, 1, prior_sigma_1=0.001))  # Output Layer
+        self.layers.append(BayesianLinear(hidden_size, 1, prior_sigma_1=prior_sigma))  # Output Layer
 
     def forward(self, x):
         for layer in self.layers[:-1]: # Apply all layers except for the last with ReLU activation
--- main.py
+++ main.py
@@ -17,40 +17,40 @@ from sklearn.decomposition import PCA
 from torch.utils.tensorboard import SummaryWriter
 
 class RunModel:
-    def __init__(self, model_name, hidden_size, layer_number, steps, epochs, dataset_type, sensor, scaling, samples_per_step, 
-                 validation_size, learning_rate, active_learning, directory, verbose, run_name):
+    def __init__(self, model_name, hidden_size, layer_number, steps, epochs, dataset_type, sensor, scaling, samples_per_step, # 0. Initialize all parameters and dataset
+                 validation_size, learning_rate, active_learning, directory, verbose, run_name, complexity_weight, prior_sigma):
 
         # Configs
         self.run_name = run_name # Name of the run
         self.verbose = verbose # Print outputs
         os.makedirs(directory, exist_ok=True) # Directory to save the outputs
-        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S") # Unique directory based on datetime for each run
-        log_dir = os.path.join('Models/runs', run_name + '_' + current_time)
+        #current_time = datetime.datetime.now().strftime("%m_%d-%H_%M_%S") # Unique directory based on datetime for each run
+        log_dir = os.path.join('Models/runs', model_name, run_name) # + '_' + current_time
         self.writer = SummaryWriter(log_dir) # TensorBoard
         print('Run saved under:', log_dir)
 
+        # Data parameters
         self.data = Dataprep(dataset_type, sensor, scaling=scaling, initial_samplesize=samples_per_step)
         self.known_data, self.pool_data = self.data.known_data, self.data.pool_data
-
-        self.validation_size = validation_size # Size of the validation set in percentage
 
         # Active learning parameters
         self.active_learning = active_learning # Whether to use active learning or random sampling
         self.steps = steps # Number of steps for the active learning
         self.epochs = epochs # Number of epochs per step of active learning
+        self.validation_size = validation_size # Size of the validation set in percentage
 
         # Initialize the model and optimizer
         self.model_name = model_name # Name of the model
-        input_dim = self.known_data.shape[1]-1 # Input dimension 
-        self.model = self.init_model(input_dim, hidden_size, layer_number) # Initialize the model
+        self.model = self.init_model(self.known_data.shape[1]-1, hidden_size, layer_number, prior_sigma) # Initialize the model
         self.criterion = torch.nn.MSELoss() # Loss function
         self.optimizer = self.init_optimizer(learning_rate) # Initialize the optimizer
         self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-        print('Using device:', self.device)
+        print(f'Using {self.device} for training')
+        self.complexity_weight = complexity_weight # Complexity weight for ELBO
 
-    def init_model(self, input_dim, hidden_size, layer_number):
+    def init_model(self, input_dim, hidden_size, layer_number, prior_sigma): # 0.1 Initialize the model
         if self.model_name == 'BNN':
-            model = BayesianNetwork(input_dim, hidden_size, layer_number)
+            model = BayesianNetwork(input_dim, hidden_size, layer_number, prior_sigma)
         # elif self.model_name == 'MC Dropout':
         #     model = MCDropout(hidden_size)
         # elif self.model_name == 'Deep Ensembles':
@@ -59,11 +59,11 @@ class RunModel:
             raise ValueError('Invalid model type')
         return model
     
-    def init_optimizer(self, learning_rate):
+    def init_optimizer(self, learning_rate): # 0.2 Initialize the optimizer
         optimizer = Adam(self.model.parameters(), lr=learning_rate)
         return optimizer
     
-    def train_model(self, step):
+    def train_model(self, step): # 1. Train the model
         # Split the pool data into train and validation sets
         if self.validation_size > 0:
             train, val = train_test_split(self.known_data, test_size=self.validation_size)
@@ -71,7 +71,6 @@ class RunModel:
             val_loader = load_data(val)
         elif self.validation_size == 0:
             train_loader = load_data(self.known_data)
-            print("No validation set")
         else:
             raise ValueError('Invalid validation size')
 
@@ -87,7 +86,7 @@ class RunModel:
 
                 loss = self.model.sample_elbo(inputs=x, labels=y,
                                               criterion=self.criterion, sample_nbr=1,
-                                              complexity_cost_weight=0) #0.01/len(train_loader.dataset)
+                                              complexity_cost_weight=self.complexity_weight) #0.01/len(train_loader.dataset)
                 loss.backward()
                 self.optimizer.step()
                 total_train_loss += loss.item()
@@ -107,7 +106,7 @@ class RunModel:
         if self.validation_size > 0:
             self.evaluate_val_data(val_loader, step) # Evaluate the model on the validation set
 
-    def evaluate_val_data(self, val_loader, step): # 1: EVALUATE THE MODEL ON THE VALIDATION SET
+    def evaluate_val_data(self, val_loader, step): # 1.1 Evaluate the model on the validation set
         self.model.eval()  # Set the model to evaluation mode
         total_val_loss = 0
 
@@ -117,28 +116,15 @@ class RunModel:
                 # Calculate loss using sample_elbo for Bayesian inference
                 loss = self.model.sample_elbo(inputs=x, labels=y,
                                             criterion=self.criterion, sample_nbr=3,
-                                            complexity_cost_weight=0) #0.01/len(val_loader.dataset)
+                                            complexity_cost_weight=self.complexity_weight)
                 total_val_loss += loss.item()
 
         step_val_loss = total_val_loss / len(val_loader) # Average loss over batches
         self.writer.add_scalar('loss/val', step_val_loss, step+1)
         if self.verbose:
             print('Step: {} | Val-Loss: {:.4f}'.format(step+1, step_val_loss))
-    
-    def predict(self, samples=1000): # 2: PREDICT THE UNCERTAINTY ON THE POOL DATA
-        self.model.eval()  # Set model to evaluation mode
-
-        x_pool = self.pool_data[:, :-1]
-        x_pool_torch = torch.tensor(x_pool).to(self.device)
-        with torch.no_grad():
-            preds = [self.model(x_pool_torch) for _ in range(samples)]
-        preds = torch.stack(preds)  # Shape: [samples, N, output_dim]
-        means = preds.mean(dim=0).detach().cpu().numpy()  # calculate the mean of the predictions
-        stds = preds.std(dim=0).detach().cpu().numpy()  # calculate the standard deviation of the predictions
-
-        return means, stds  # Return both the mean and standard deviation of predictions
-    
-    def evaluate_pool_data(self, step):
+        
+    def evaluate_pool_data(self, step): # 2. Evaluate the model on the pool data
         # Convert pool data to PyTorch tensors and move them to the correct device
         x_pool_torch = torch.tensor(self.pool_data[:, :-1]).to(self.device)
         y_pool_torch = torch.tensor(self.pool_data[:, -1]).unsqueeze(1).to(self.device) #.view(-1, 1).to(self.device)  # Ensure y_pool is the correct shape
@@ -153,26 +139,33 @@ class RunModel:
         
         if self.verbose:
             print(f'Step: {step + 1} | Pool-Loss: {loss.item()}')
+    
+    def predict(self, samples=500): # 3. Predict the mean and std (uncertainty) on the pool data
+        self.model.eval()  # Set model to evaluation mode
 
-    def acquisition_function(self, means, stds, samples_per_step, step): # 3: DECIDE WHICH SAMPLES TO SELECT FROM THE POOL DATA (USING ACQUISITION FUNCTION)
+        x_pool = self.pool_data[:, :-1]
+        x_pool_torch = torch.tensor(x_pool).to(self.device)
+        with torch.no_grad():
+            preds = [self.model(x_pool_torch) for _ in range(samples)]
+        preds = torch.stack(preds)  # Shape: [samples, N, output_dim]
+        means = preds.mean(dim=0).detach().cpu().numpy()  # calculate the mean of the predictions
+        stds = preds.std(dim=0).detach().cpu().numpy()  # calculate the standard deviation of the predictions
+
+        self.preds_csv(preds, 'prediction')
+
+        return means, stds  # Return both the mean and standard deviation of predictions
+
+    def acquisition_function(self, means, stds, samples_per_step, step): # 4. Select the next samples from the pool
         if self.active_learning:
             uncertainty = stds.squeeze() # Uncertainty is the standard deviation of the predictions
             selected_indices = uncertainty.argsort()[-samples_per_step:] # Select the indices with the highest uncertainty
             return selected_indices
-            if self.known_data.shape[1] == 2:
-                self.plot(means, stds, selected_indices, step) # Plot the predictions and selected indices
             
         else:
             selected_indices = random.sample(range(len(self.pool_data)), samples_per_step)
             return selected_indices
-            if self.known_data.shape[1] == 2:
-                self.plot(means, stds, selected_indices, step) # Plot the predictions and selected indices
             
-    def update_data(self, selected_indices): # Based on the indices passed, transfer the data from pool to known
-        self.known_data = np.append(self.known_data, self.pool_data[selected_indices], axis=0)
-        self.pool_data = np.delete(self.pool_data, selected_indices, axis=0)
-
-    def plot(self, means, stds, selected_indices, step):
+    def plot(self, means, stds, selected_indices, step): #4.1 Plot the predictions and selected indices
         x_pool = self.pool_data[:, :-1] # [observations, features]
         y_pool = self.pool_data[:, -1] # [observations]
         x_selected = self.known_data[:, :-1] # [observations, features]
@@ -188,7 +181,7 @@ class RunModel:
         x_pool_selected = x_pool[selected_indices] # [observations, 1]
         y_pool_selected = y_pool[selected_indices] # [observations]
 
-        y_vals = [means, means + 100 * stds, means - 100 * stds] #list of 3 arrays of shape [observations in pool, 1] (use 2 for 95% CI)
+        y_vals = [means, means + 200 * stds, means - 200 * stds] #list of 3 arrays of shape [observations in pool, 1] (use 2 for 95% CI)
         df = pd.concat([pd.DataFrame({'x': x_pool.squeeze(), 'y': y_val.squeeze()}) for y_val in y_vals], ignore_index=True)
 
         # Plotting
@@ -220,29 +213,53 @@ class RunModel:
             
             self.writer.add_figure(f'First two PCs with selected datapoints {step + 1}', fig, step + 1)
 
+    def update_data(self, selected_indices): # 5. Update the known and pool data
+        self.known_data = np.append(self.known_data, self.pool_data[selected_indices], axis=0)
+        self.pool_data = np.delete(self.pool_data, selected_indices, axis=0)
+
+    def preds_csv(self, preds, name):
+        preds = preds.detach().cpu().numpy()
+        preds = preds.reshape(-1, preds.shape[-1])
+        preds = pd.DataFrame(preds)
+        preds.to_csv(f'{name}_preds_{self.run_name}.csv', index=False)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the BNN model')
-    parser.add_argument('-m', '--model', type=str, default='BNN', help='1. BNN, 2. MC Dropout, 3. Deep Ensembles')
-    parser.add_argument('-hs', '--hidden_size', type=int, default=4, help='Number of hidden units')
-    parser.add_argument('-ln', '--layer_number', type=int, default=3, help='Number of layers')
-    parser.add_argument('-s', '--steps', type=int, default=2, help='Number of steps')
-    parser.add_argument('-e', '--epochs', type=int, default=20, help='Number of epochs')
+    # Dataset parameters
     parser.add_argument('-ds', '--dataset_type', type=str, default='Generated_2000', help='1. Generated_2000, 2. Caselist')
+    parser.add_argument('-sc', '--scaling', type=str, default='Standard', help='Scaling to be used: 1. Standard, 2. Minmax, 3. None')
     parser.add_argument('-se', '--sensor', type=str, default='foundation_origin xy FloaterOffset [m]', help='Sensor to be predicted')
-    parser.add_argument('-sc', '--scaling', type=str, default='none', help='Scaling to be used: 1. standard, 2. minmax, 3. none')
-    parser.add_argument('-is', '--samples_per_step', type=int, default=100, help='Samples to be selected per step and initial samplesize')
-    parser.add_argument('-vs', '--validation_size', type=float, default=0, help='Size of the validation set in percentage')
+    
+    # Model parameters
+    parser.add_argument('-m', '--model', type=str, default='BNN', help='1. BNN, 2. MC Dropout, 3. Deep Ensembles')
+    parser.add_argument('-hs', '--hidden_size', type=int, default=10, help='Number of hidden units')
+    parser.add_argument('-ln', '--layer_number', type=int, default=10, help='Number of layers')
+    parser.add_argument('-ps', '--prior_sigma', type=float, default=0.1, help='Prior sigma')
+    parser.add_argument('-cw', '--complexity_weight', type=float, default=0.1, help='Complexity weight')
     parser.add_argument('-lr', '--learning_rate', type=float, default=0.01, help='Learning rate')
+    
+    # Active learning parameters
     parser.add_argument('-al', '--active_learning', action='store_true', help='Use active learning (AL) or random sampling (RS)')
+    parser.add_argument('-s', '--steps', type=int, default=2, help='Number of steps')
+    parser.add_argument('-e', '--epochs', type=int, default=100, help='Number of epochs')
+    parser.add_argument('-ss', '--samples_per_step', type=int, default=100, help='Samples to be selected per step and initial samplesize')
+    parser.add_argument('-vs', '--validation_size', type=float, default=0, help='Size of the validation set in percentage')
+    
+    # Output parameters    
     parser.add_argument('-dr', '--directory', type=str, default='_plots', help='Directory to save the ouputs')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print the model')
+    parser.add_argument('-rn', '--run_name', type=str, default='', help='Run name prefix')
+    
     args = parser.parse_args()
 
-    run_name = args.model + '_' + args.dataset_type + '_scaling=' + args.scaling + '_AL=' + str(args.active_learning) + '_Steps=' + str(args.steps) + '_Epochs=' + str(args.epochs)
-
+    outside = ['-h', '-ds', '-m', '-v', '-rn', '-dr', '-vs', '-se']
+    option_value_list = [(action.option_strings[0].lstrip('-'), getattr(args, action.dest)) 
+                         for action in parser._actions if action.option_strings and action.option_strings[0] not in outside]
+    run_name = args.run_name + '_' + '_'.join(f"{abbr}{str(value)}" for abbr, value in option_value_list)
+    
     model = RunModel(args.model, args.hidden_size, args.layer_number, args.steps, args.epochs, args.dataset_type, args.sensor, args.scaling, args.samples_per_step, 
-                     args.validation_size, args.learning_rate, args.active_learning, args.directory, args.verbose, run_name)
+                     args.validation_size, args.learning_rate, args.active_learning, args.directory, args.verbose, run_name, args.complexity_weight, args.prior_sigma)
 
     # Iterate through the steps of active learning
     for step in range(model.steps):
@@ -251,9 +268,8 @@ if __name__ == '__main__':
         model.evaluate_pool_data(step) # Evaluate the model on the pool data
         means, stds = model.predict() # Predict the uncertainty on the pool data
         selected_indices = model.acquisition_function(means, stds, args.samples_per_step, step) # Select the next samples from the pool
-        #if args.dataset_type == 'Generated_2000':
         model.plot(means, stds, selected_indices, step) # Plot the predictions and selected indices
         model.update_data(selected_indices) # Update the known and pool data
 
-        print(f'After updating the pool and known data using AL = {args.active_learning}:', model.pool_data.shape, model.known_data.shape)
+        print(f'Updated pool and known data (AL = {args.active_learning}):', model.pool_data.shape, model.known_data.shape)
         print('Step: {} of {} | {:.2f} seconds'.format(step+1, model.steps, time.time() - start_step))
