c94d264fdf5626c8027d629043fda4aad5cd1dfc
FinnFe0103 <fife22ab@student.cbs.dk>
2024-03-18 10:53:29 +0100

    Framework Clean Up

--- Models/ExactGP.py
+++ Models/ExactGP.py
@@ -1,5 +1,4 @@
 import gpytorch
-import torch 
 
 class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood, kernel='RBF'):
--- main.py
+++ main.py
@@ -29,8 +29,8 @@ class RunModel:
         self.run_name = run_name # Name of the run
         self.verbose = verbose # Print outputs
         os.makedirs('_plots', exist_ok=True) # Directory to save the outputs
-        #current_time = datetime.datetime.now().strftime("%m_%d-%H_%M_%S") # Unique directory based on datetime for each run
-        log_dir = os.path.join('Models/runs', model_name, directory, run_name) # + '_' + current_time
+        current_time = datetime.datetime.now().strftime("%H%M%S") # Unique directory based on datetime for each run
+        log_dir = os.path.join('Models/runs', model_name, directory, run_name + '_' + current_time)
         self.writer = SummaryWriter(log_dir) # TensorBoard
         print('Run saved under:', log_dir)
 
@@ -64,15 +64,12 @@ class RunModel:
             model = BayesianNetwork(input_dim, hidden_size, layer_number, prior_sigma)
         elif self.model_name == 'GP':
             model = None #model needs to be initialized with the data
-        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-        print(f'Using {self.device} for training')
-        self.complexity_weight = complexity_weight # Complexity weight for ELBO
     
     def init_optimizer(self, learning_rate):
         if self.model == None: # GP model needs to initialize with the data, then optimizer can be initialized
             return None 
         else:
-            optimizer = Adam(self.model.parameters(), lr=learning_rate)
+            self.optimizer = Adam(self.model.parameters(), lr=learning_rate)
     
     def train_model(self, step): # 1. Train the model
         # Split the pool data into train and validation sets
@@ -104,7 +101,7 @@ class RunModel:
             self.likelihood = gpytorch.likelihoods.GaussianLikelihood().to(self.device)
             self.likelihood.noise = 0.01 #hyperparameter
             self.model = ExactGPModel(X_train, y_train, self.likelihood).to(self.device)
-            self.optimizer = self.init_optimizer(self.learning_rate)
+            self.init_optimizer(self.learning_rate)
 
             self.model.train()
             self.likelihood.train()
@@ -114,11 +111,15 @@ class RunModel:
                 start_epoch = time.time()
                 self.optimizer.zero_grad()
                 output = self.model(X_train)
-                loss = -self.mll(output, y_train) 
-                loss.backward()
-                if self.verbose:
-                    print(f'Step: {step+1} | Epoch: {epoch+1} of {self.epochs} | Train-Loss: {loss:.4f} | Lengthscale: {self.model.covar_module.base_kernel.lengthscale.item():.3f} | Noise: {self.likelihood.noise.item():.3f} | {time.time() - start_epoch:.2f} seconds')
+                train_loss = -self.mll(output, y_train) 
+                train_loss.backward()
                 self.optimizer.step()
+
+                self.writer.add_scalar('loss/train', train_loss, step*self.epochs+epoch+1)
+                if self.verbose:
+                    print(f'Step: {step+1} | Epoch: {epoch+1} of {self.epochs} | Train-Loss: {train_loss:.4f} | Lengthscale: {self.model.covar_module.base_kernel.lengthscale.item():.3f} | Noise: {self.likelihood.noise.item():.3f} | {time.time() - start_epoch:.2f} seconds')
+                
+
             if self.validation_size > 0:
                 # Evaluation step here, assuming evaluate_model method exists
                 self.evaluate_val_data(val_loader, step)
@@ -152,7 +153,7 @@ class RunModel:
                 epoch_train_loss = total_train_loss / len(train_loader) # Average loss over batches
                 self.writer.add_scalar('loss/train', epoch_train_loss, step*self.epochs+epoch+1)
                 if self.verbose:
-                    print('Step: {} | Epoch: {} of {} | Train-Loss: {:.4f} | {:.2f} seconds'.format(step+1, epoch+1, self.epochs, epoch_train_loss, time.time() - start_epoch))
+                    print(f'Step: {step+1} | Epoch: {epoch+1} of {self.epochs} | Train-Loss: {epoch_train_loss:.4f} | {time.time() - start_epoch:.2f} seconds')
 
             if self.validation_size > 0:
                 self.evaluate_val_data(val_loader, step) # Evaluate the model on the validation set
@@ -166,16 +167,14 @@ class RunModel:
             X_val = torch.tensor(val_loader[:, :-1]).to(self.device)
             y_val = torch.tensor(val_loader[:, -1]).to(self.device)
             
-            total_val_loss = 0
-            with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            with torch.no_grad(), gpytorch.settings.fast_pred_var(): # There is only one "batch" for GP validation
                 output = self.model(X_val)
                 loss = -self.mll(output, y_val)
-                total_val_loss += loss.item()
+                total_val_loss = loss.item()
 
-            step_val_loss = total_val_loss  # There is only one "batch" for GP validation
-            self.writer.add_scalar('loss/val', step_val_loss, step+1)
+            self.writer.add_scalar('loss/val', total_val_loss, step+1)
             if self.verbose:
-                print(f'Step: {step+1}, Val-Loss: {step_val_loss:.4f}')
+                print(f'Step: {step+1}, Val-Loss: {total_val_loss:.4f}')
         
         else:
             self.model.eval()  # Set the model to evaluation mode
@@ -193,7 +192,7 @@ class RunModel:
             step_val_loss = total_val_loss / len(val_loader) # Average loss over batches
             self.writer.add_scalar('loss/val', step_val_loss, step+1)
             if self.verbose:
-                print('Step: {} | Val-Loss: {:.4f}'.format(step+1, step_val_loss))
+                print(f'Step: {step+1} | Val-Loss: {step_val_loss:.4f}')
         
     def evaluate_pool_data(self, step): # 2. Evaluate the model on the pool data
         # Convert pool data to PyTorch tensors and move them to the correct device
@@ -207,7 +206,7 @@ class RunModel:
                 self.likelihood.eval()  # Also set the likelihood to evaluation mode
                 prediction = self.model(x_pool_torch.unsqueeze(0))  # Add unsqueeze to match expected dimensions
                 predictions = self.likelihood(prediction)  # Get the predictive posterior
-                
+
                 # Compute MSE Loss for GP
                 loss = self.criterion(predictions.mean.reshape(-1, 1), y_pool_torch) 
 
@@ -226,20 +225,20 @@ class RunModel:
 
         x_pool = self.pool_data[:, :-1]
         x_pool_torch = torch.tensor(x_pool).to(self.device)
-        with torch.no_grad():
           
-            if self.model_name == 'GP':
-                with torch.no_grad(), gpytorch.settings.fast_pred_var():
-                    self.likelihood.eval()  # Also set the likelihood to evaluation mode for GP predictions
-                    preds = self.model(x_pool_torch)
-                    observed_pred = self.likelihood(preds)
-                    means = observed_pred.mean.numpy()
-                    stds = observed_pred.stddev.numpy()
-            else:
+        if self.model_name == 'GP':
+            with torch.no_grad(), gpytorch.settings.fast_pred_var():
+                self.likelihood.eval()  # Also set the likelihood to evaluation mode for GP predictions
+                preds = self.model(x_pool_torch)
+                observed_pred = self.likelihood(preds)
+            means = observed_pred.mean.numpy()
+            stds = observed_pred.stddev.numpy()
+        else:
+            with torch.no_grad():
                 preds = [self.model(x_pool_torch) for _ in range(samples)]
-                preds = torch.stack(preds)  # Shape: [samples, N, output_dim]
-                means = preds.mean(dim=0).detach().cpu().numpy()  # calculate the mean of the predictions
-                stds = preds.std(dim=0).detach().cpu().numpy()  # calculate the standard deviation of the predictions
+            preds = torch.stack(preds)  # Shape: [samples, N, output_dim]
+            means = preds.mean(dim=0).detach().cpu().numpy()  # calculate the mean of the predictions
+            stds = preds.std(dim=0).detach().cpu().numpy()  # calculate the standard deviation of the predictions
 
         #self.preds_csv(preds, 'prediction')
 
@@ -266,6 +265,7 @@ class RunModel:
             pca = PCA(n_components=1)
             x_pool = pca.fit_transform(x_pool) # [observations, 1]
             x_selected = pca.transform(x_selected) # [observations, 1]
+            x_highest = pca.transform(x_highest) # [observations, 1]
             pca_applied = True
             
         x_pool_selected = x_pool[selected_indices] # [observations, 1]
@@ -283,7 +283,7 @@ class RunModel:
         plt.scatter(x_highest, y_highest, c="purple", marker="o", alpha=0.3)
         plt.title(self.run_name.replace("_", " ") + f' | Step {step + 1}', fontsize='small')
         plt.xlabel('1 Principal Component' if pca_applied else 'x')
-        plt.legend(['Mean prediction', 'Confidence Interval', 'Pool data (unseen)', 'Seen data', 'Selected data'], fontsize='small')
+        plt.legend(['Mean prediction', 'Confidence Interval', 'Pool data (unseen)', 'Seen data', 'Selected data', 'Final Prediction'], fontsize='small')
         plt.close(fig)
 
         # Log the table figure
@@ -314,40 +314,32 @@ class RunModel:
         preds = pd.DataFrame(preds)
         preds.to_csv(f'{name}_preds_{self.run_name}.csv', index=False)
 
-    def final_prediction(self, step, topk=5, samples=500):
+    def final_prediction(self, step, topk, samples=500):
         self.model.eval()
         x_total = np.concatenate((self.pool_data[:, :-1], self.known_data[:, :-1]), axis=0)
         y_total = np.concatenate((self.pool_data[:, -1], self.known_data[:, -1]), axis=0)
         
         x_total_torch = torch.tensor(x_total).to(self.device)
-        with torch.no_grad():
-            if self.model_name == 'GP':
-                with torch.no_grad(), gpytorch.settings.fast_pred_var():
-                    self.likelihood.eval()  # Also set the likelihood to evaluation mode for GP predictions
-                    preds = self.model(x_total_torch)
-                    observed_pred = self.likelihood(preds)
-                    means = observed_pred.mean.numpy()
-                    highest_indices = np.argsort(means)[-topk:]
-            else:
+        
+        if self.model_name == 'GP':
+            with torch.no_grad(), gpytorch.settings.fast_pred_var():
+                self.likelihood.eval()  # Also set the likelihood to evaluation mode for GP predictions
+                preds = self.model(x_total_torch)
+                observed_pred = self.likelihood(preds)
+            means = observed_pred.mean.numpy()
+            highest_indices = np.argsort(means)[-topk:]
+        else:
+            with torch.no_grad():
                 preds = [self.model(x_total_torch) for _ in range(samples)]
-                preds = torch.stack(preds)
-                means = preds.mean(dim=0).detach().cpu().numpy().squeeze()
-                highest_indices = np.argsort(means)[-topk:]
+            preds = torch.stack(preds)
+            means = preds.mean(dim=0).detach().cpu().numpy().squeeze()
+            highest_indices = np.argsort(means)[-topk:]
 
         x_highest = x_total[highest_indices]
         y_highest = y_total[highest_indices]
 
-        # fig = plt.figure()
-        # plt.scatter(self.pool_data[:, :-1], self.pool_data[:, -1], c="green", marker="*", alpha=0.1)  # Plot the data pairs in the pool
-        # plt.scatter(self.known_data[:, :-1], self.known_data[:, -1], c="red", marker="*", alpha=0.2)  # plot the train data on top
-        # plt.scatter(x_highest, y_highest, c="blue", marker="o", alpha=0.3)  # Highlight selected data points
-        # plt.title(self.run_name.replace("_", " ") + f' | Step {step + 1}', fontsize='small')
-        # plt.legend(['Pool Data', 'Known Data', '100 Highest Preds'], fontsize='small')
-        # #plt.close(fig)
-        # plt.show()
         return x_highest, y_highest
     
-
 
 if __name__ == '__main__':
 
@@ -358,19 +350,22 @@ if __name__ == '__main__':
     parser.add_argument('-se', '--sensor', type=str, default='foundation_origin xy FloaterOffset [m]', help='Sensor to be predicted')
     
     # Model parameters
-    parser.add_argument('-m', '--model', type=str, default='BNN', help='1. BNN, 2. MC Dropout, 3. Deep Ensembles')
+    parser.add_argument('-m', '--model', type=str, default='BNN', help='1. BNN, 2. GP, 3. MCD, 4. DE, 5. SVM')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.01, help='Learning rate')
+    
+    # BNN
     parser.add_argument('-hs', '--hidden_size', type=int, default=4, help='Number of hidden units')
     parser.add_argument('-ln', '--layer_number', type=int, default=3, help='Number of layers')
     parser.add_argument('-ps', '--prior_sigma', type=float, default=0.0000001, help='Prior sigma')
     parser.add_argument('-cw', '--complexity_weight', type=float, default=0.01, help='Complexity weight')
-    parser.add_argument('-lr', '--learning_rate', type=float, default=0.01, help='Learning rate')
-    
+
     # Active learning parameters
     parser.add_argument('-al', '--active_learning', action='store_true', help='Use active learning (AL) or random sampling (RS)')
     parser.add_argument('-s', '--steps', type=int, default=2, help='Number of steps')
     parser.add_argument('-e', '--epochs', type=int, default=100, help='Number of epochs')
     parser.add_argument('-ss', '--samples_per_step', type=int, default=100, help='Samples to be selected per step and initial samplesize')
     parser.add_argument('-vs', '--validation_size', type=float, default=0, help='Size of the validation set in percentage')
+    parser.add_argument('-t', '--topk', type=int, default=100, help='Number of top predictions to be selected')
     
     # Output parameters    
     parser.add_argument('-dr', '--directory', type=str, default='_plots', help='Sub-directory to save the ouputs')
@@ -378,21 +373,22 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-    outside = ['-h', '-ds', '-m', '-v', '-rn', '-dr', '-vs', '-se'] 
+    if args.model == 'BNN':
+        opt_list = ['-sc', '-hs', '-ln', '-ps', '-cw', '-lr', '-al', '-s', '-e', '-ss', '-vs']
+    elif args.model == 'GP':
+        opt_list = ['-sc', '-lr', '-al', '-s', '-e', '-ss', '-vs']
     option_value_list = [(action.option_strings[0].lstrip('-'), getattr(args, action.dest)) 
-                         for action in parser._actions if action.option_strings and action.option_strings[0] not in outside]
+                         for action in parser._actions if action.option_strings and action.option_strings[0] in opt_list]
     run_name = '_'.join(f"{abbr}{str(value)}" for abbr, value in option_value_list)
     
     model = RunModel(args.model, args.hidden_size, args.layer_number, args.steps, args.epochs, args.dataset_type, args.sensor, args.scaling, args.samples_per_step, 
                      args.validation_size, args.learning_rate, args.active_learning, args.directory, args.verbose, run_name, args.complexity_weight, args.prior_sigma)
-    # BNN: -v -ln 3 -hs 4 -ps 0.0000001 -cw 0.01 -dr v1
-    # GP Demo: -v -m GP -sc Minmax -lr 0.1 -al -s 10 -e 10 -ss 25 -vs 0.2 
 
     # Iterate through the steps of active learning
     for step in range(model.steps):
         start_step = time.time()
         model.train_model(step) # Train the model
-        x_highest, y_highest = model.final_prediction(step)
+        x_highest, y_highest = model.final_prediction(step, topk=args.topk)
         model.evaluate_pool_data(step) # Evaluate the model on the pool data
         means, stds = model.predict() # Predict the uncertainty on the pool data
         selected_indices = model.acquisition_function(means, stds, args.samples_per_step, step) # Select the next samples from the pool
@@ -400,4 +396,7 @@ if __name__ == '__main__':
         model.update_data(selected_indices) # Update the known and pool data
 
         print(f'Updated pool and known data (AL = {args.active_learning}):', model.pool_data.shape, model.known_data.shape)
-        print('Step: {} of {} | {:.2f} seconds'.format(step+1, model.steps, time.time() - start_step))
+        print(f'Step: {step+1} of {model.steps} | {time.time() - start_step:.2f} seconds')
+
+# BNN: -v -ln 3 -hs 4 -ps 0.0000001 -cw 0.01 -dr v1
+# GP Demo: -v -m GP -sc Minmax -lr 0.1 -al -s 10 -e 10 -ss 25 -vs 0.2 
